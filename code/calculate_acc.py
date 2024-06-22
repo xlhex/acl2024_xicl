46c527e900ad600a9f201b34a085a46d0ea47743
Zodiac He <zodiachy@gmail.com>
2024-06-22 13:00:03 +0100

    update code

---
+++ code/calculate_acc.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python
+#-*-coding:utf-8 -*-
+#Author   : Xuanli He
+#Version  : 1.0
+from __future__ import print_function
+
+import sys
+import re
+
+def main(input_file):
+
+    total = 0
+    correct = 0
+    with open(input_file) as reader:
+        for i, line in enumerate(reader):
+            if not line.startswith("{'original_prompt':"): continue
+            items = eval(line)
+            response = items["output"].strip().split("###")[0].strip()
+            total += 1
+            for line in response.split("\n"):
+                line = line.strip()
+                if line.startswith("Label:"):
+                    print(line.replace("Label: ", ""), items["answer"])
+                    if line.replace("Label: ", "") == items["answer"]:
+                        correct += 1
+                    if items["answer"] == "not entailment":
+                        if line.replace("Label: ", "") in ["contradiction", "neutral"]:
+                            correct += 1
+                    break
+
+    print(correct, total, round(correct/total*100, 2))
+
+if __name__ == "__main__":
+    main(sys.argv[1])
+
